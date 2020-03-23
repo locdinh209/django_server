7c86635515b64ca165014d18fdec1e54392be25c
LocDinh_209 <locdinh.pk209@gmail.com>
2020-03-23 11:39:51 +0700

    endpoints models

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add apps endpoint
+    'apps.endpoints',
+    # add django rest framework
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += endpoints_urlpatterns
