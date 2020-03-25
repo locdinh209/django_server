f2a7dc5938a66474e8893d1458ee8de20db50f14
LocDinh_209 <locdinh.pk209@gmail.com>
2020-03-25 14:03:29 +0700

    add ab testing

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -2,8 +2,32 @@ from django.test import TestCase
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 
 class MLTests(TestCase):
+    def test_et_algorithm(self):
+        input_data = {
+            "age": 37,
+            "workclass": "Private",
+            "fnlwgt": 34146,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Craft-repair",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 68,
+            "native-country": "United-States"
+        }
+        my_alg = ExtraTreesClassifier()
+        response = my_alg.compute_prediction(input_data)
+        self.assertEqual('OK', response['status'])
+        self.assertTrue('label' in response)
+        self.assertEqual('<=50K', response['label'])
+
     def test_rf_algorithm(self):
         input_data = {
             "age": 37,
--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -8,6 +8,7 @@ application = get_wsgi_application()
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier # import ExtraTrees ML algorithm
 
 try:
     registry = MLRegistry() # create ML registry
@@ -22,6 +23,18 @@ try:
                             owner="Piotr",
                             algorithm_description="Random Forest with simple pre- and post-processing",
                             algorithm_code=inspect.getsource(RandomForestClassifier))
+
+    # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                            algorithm_object=et,
+                            algorithm_name="extra trees",
+                            algorithm_status="testing",
+                            algorithm_version="0.0.1",
+                            owner="Piotr",
+                            algorithm_description="Extra Trees with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(ExtraTreesClassifier))
 
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
